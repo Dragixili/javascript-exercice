2e8785027e981b2c473e544d3ce8f48fa3eecba0
Arthur <arthurdionlamant2008@gmail.com>
2026-01-23 19:13:44 +0100

    modifie inex.html index.js

--- Pomodoro/index.js
+++ Pomodoro/index.js
@@ -2,6 +2,26 @@ document.addEventListener("DOMContentLoaded", (event) => {
     const commencer = document.querySelector(".play");
     commencer.addEventListener("click", start);
     function start(){
-        alert("commencer");
+        const minutes = document.querySelector(".min");
+        const secondes = document.querySelector(".sec")
+        console.log(minutes.innerHTML);
+        console.log(secondes.innerHTML);
+        let valeur = parseInt(minutes.innerHTML) * 60 + parseInt(secondes.innerHTML);
+        console.log(valeur);
+        let temps = valeur;
+        
+        
+        function diminuerTemps(){
+            let minute = parseInt(temps / 60, 10);
+            let seconde = parseInt(temps % 60, 10);
+
+            minute = minute < 10 ? "0" + minute : minute;
+            seconde = seconde < 10 ? "0" + seconde : seconde; 
+
+            minutes.innerHTML = minute;
+            secondes.innerHTML = seconde;
+            temps--;
+        }
+        setInterval(diminuerTemps,1000);
     }
 });
