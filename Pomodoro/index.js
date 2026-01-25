67f7768a8052ab016a6bd99b647c872f0d65c466
Dragxili <arthurdionlamant2008@gmail.com>
2026-01-25 16:48:15 +0100

    modifie index.js

--- Pomodoro/index.js
+++ Pomodoro/index.js
@@ -1,15 +1,16 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     const commencer = document.querySelector(".play");
     commencer.addEventListener("click", start);
+
+    let number_click = 0;
+    let intervalID;
+
     function start(){
         const minutes = document.querySelector(".min");
         const secondes = document.querySelector(".sec")
-        console.log(minutes.innerHTML);
-        console.log(secondes.innerHTML);
         let valeur = parseInt(minutes.innerHTML) * 60 + parseInt(secondes.innerHTML);
-        console.log(valeur);
         let temps = valeur;
-        
+        number_click += 1;
         
         function diminuerTemps(){
             let minute = parseInt(temps / 60, 10);
@@ -20,8 +21,20 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
             minutes.innerHTML = minute;
             secondes.innerHTML = seconde;
-            temps--;
+            temps = temps <= 0 ? 0 : temps - 1;
         }
-        setInterval(diminuerTemps,1000);
+        
+        function start_interval(){
+            intervalID ??= setInterval(diminuerTemps, 1000);
+        }
+        function stop_interval(){
+            clearInterval(intervalID);
+            intervalID = null;
+        }
+        if(number_click % 2 === 1){
+            start_interval();
+        } else {
+            stop_interval();
+        }
     }
 });
